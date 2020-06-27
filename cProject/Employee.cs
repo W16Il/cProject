3c02c42aa6bf35a98ab427d77b0d3334734949cf
Wilheln <wilhelnhans@gmail.com>
2020-06-26 20:30:47 -0600

    Primero Stage

---
+++ cProject/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security;
+
+namespace cProject
+{
+    public class Employee
+    {
+        public void startWindow()
+        {
+            var opt = " ";
+            while (opt != "X")
+            {
+                getMenu();
+                opt = getOpt();
+            }
+        }
+
+        private string getOpt()
+        {
+            var opt = Console.ReadLine();
+            return opt;
+        }
+
+        private void getMenu()
+        {
+            Console.WriteLine("1. Listar Empleados");
+            Console.WriteLine("X. Salir");
+
+        }
+    }
+}
---
+++ cProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoJob();
+        }
+
+        static void DoJob()
+        {
+            var empleado = new Employee();
+            empleado.startWindow();
+        }
+    }
+}
